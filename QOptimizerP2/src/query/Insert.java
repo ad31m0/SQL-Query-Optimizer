8cb02ea6c25d1c5b04adab53e731ce9b79b768f8
Belbesy <mohmdadel@gmail.com>
2013-05-08 14:18:24 +0200

    insert added

--- QOptimizerP2/src/query/Insert.java
+++ QOptimizerP2/src/query/Insert.java
@@ -1,7 +1,12 @@
 package query;
 
+import heap.HeapFile;
+import index.HashIndex;
 import global.Minibase;
+import global.RID;
+import global.SearchKey;
 import parser.AST_Insert;
+import primitives.Schema;
 import primitives.Tuple;
 
 /**
@@ -10,7 +15,9 @@ import primitives.Tuple;
 class Insert implements Plan {
 
 	private String fileName;
+	private Schema schema;
 	private Tuple tuple;
+	
 
 	/**
 	 * Optimizes the plan, given the parsed query.
@@ -21,19 +28,26 @@ class Insert implements Plan {
 	public Insert(AST_Insert tree) throws QueryException {
 		fileName = tree.getFileName();
 		QueryCheck.tableExists(fileName);
-
-		tuple = new Tuple(Minibase.SystemCatalog.getSchema(fileName),
-				tree.getValues());
-
+		schema = Minibase.SystemCatalog.getSchema(fileName);
+		QueryCheck.insertValues(schema, tree.getValues());
+		tuple = new Tuple(schema , tree.getValues());
+		//TODO check for size and types
 	} // public Insert(AST_Insert tree) throws QueryException
 
 	/**
 	 * Executes the plan and prints applicable output.
 	 */
 	public void execute() {
-
+		RID rid = new HeapFile(fileName).insertRecord(tuple.getData());
+		int[] allcols = new int[schema.getCount()];
+		for(int i=0; i<schema.getCount(); i++)
+			allcols[i] = i;
+		IndexDesc[] indexes = Minibase.SystemCatalog.getIndexes(fileName, schema, allcols);
+		for(IndexDesc index : indexes)
+			new HashIndex(index.indexName).insertEntry(new SearchKey(tuple.getField(index.columnName)), rid);
+		
 		// print the output message
-		System.out.println("0 rows affected. (Not implemented)");
+		System.out.println("1 rows inserted.");
 
 	} // public void execute()
 
